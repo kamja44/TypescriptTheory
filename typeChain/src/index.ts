3787c7a5558d8a4922d13ef7f044298a9dc4ae9e
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-02-17 23:26:24 +0900

    3.7
    create BlockChain

--- typeChain/src/index.ts
+++ typeChain/src/index.ts
@@ -19,3 +19,33 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    // return this.blocks; <- 누구든 block에 접근가능하기에 해킹당할 수 있다.
+    return [...this.blocks];
+  }
+}
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+// blockchain.getBlocks().push(new Block("xxxxxx", 111111, "Hacked!@!!"));
+console.log(blockchain.getBlocks());
